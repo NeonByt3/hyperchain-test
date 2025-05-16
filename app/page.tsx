72cbac11aea05e03b80efa07da1c0a96f315a276
dolvin17 <huertas876@gmail.com>
2025-05-16 15:05:47 +0200

    indexing new comp

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import About from "@/components/Home/About";
 import HomeSection from "@/components/Home/Home";
+import HyperchainReady from "@/components/Home/HyperchainReady";
 import ProofOfValidation from "@/components/Home/Proof";
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
       <HomeSection />
 	  <ProofOfValidation />
 	  <About	/>
+	  <HyperchainReady	/>
     </>
   );
 }
